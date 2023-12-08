d76cee3f8fbc61faa7a46a5191a4ad6f78b74598
rdeighton19 <50099383+rdeighton19@users.noreply.github.com>
2023-12-08 10:27:43 +0000

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -3,4 +3,6 @@ resource "aws_cloudtrail" "insecure-logging" {
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
   enable_log_file_validation = true
+  is_multi_region_trail attribute = true
+  cloud_watch_logs_group_arn = ""
 }
